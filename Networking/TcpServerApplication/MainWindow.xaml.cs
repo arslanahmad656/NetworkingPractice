51dfc95d382cc07787e1a0984a8287bd9a7deef9
Arslan Ahmad <ars.ahm6@gmail.com>
2019-04-26 21:30:23 +0500

    Server almost completed.

--- Networking/TcpServerApplication/MainWindow.xaml.cs
+++ Networking/TcpServerApplication/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 
 namespace TcpServerApplication
 {
@@ -25,6 +26,15 @@ namespace TcpServerApplication
     {
         private event EventHandler<ServerStatusEventsArgs> OnServerStatusChanged;
 
+        private TcpListener server;
+        private CancellationTokenSource tokenSource;
+        private bool pendingMessageToSend;
+        private string pendingMessage;
+
+        private int oldPort;
+        private const int minPortAllowed = 32000;
+        private const int maxPortAllowed = 45000;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -33,6 +43,7 @@ namespace TcpServerApplication
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             OnServerStatusChanged += (senderObj, args) => SetControlsStatus(args.CurrentStatus);
+            oldPort = int.Parse(Txt_Port.Text);
         }
 
         private void SetControlsStatus(bool serverStarted)
@@ -47,11 +58,102 @@ namespace TcpServerApplication
         private void Btn_Start_Click(object sender, RoutedEventArgs e)
         {
             OnServerStatusChanged?.Invoke(new object(), new ServerStatusEventsArgs(true));
+            tokenSource = new CancellationTokenSource();
+            Task.Run(() => StartServer(tokenSource.Token));
         }
 
-        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        private async void Btn_Stop_Click(object sender, RoutedEventArgs e)
         {
             OnServerStatusChanged?.Invoke(new object(), new ServerStatusEventsArgs(false));
+            tokenSource.Cancel();
+            server.Stop();
+            await WriteToSummary($"Server stopped.", MessageSource.Server);
+        }
+
+        private void Txt_Port_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var txtBox = sender as TextBox;
+            var text = txtBox.Text;
+            if (int.TryParse(text, out int result) && result >= minPortAllowed && result <= maxPortAllowed)
+            {
+                oldPort = result;
+            }
+            else
+            {
+                MessageBox.Show($"Invalid Port.{Environment.NewLine}Allowed ports: {minPortAllowed} - {maxPortAllowed}", "Invalid Port", MessageBoxButton.OK, MessageBoxImage.Error);
+                txtBox.Text = oldPort.ToString();
+            }
+        }
+
+        private async void StartServer(CancellationToken token)
+        {
+            try
+            {
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Loopback, oldPort);
+                server = new TcpListener(endPoint);
+                server.Start();
+                await WriteToSummary($"Server started. Listening at {server.LocalEndpoint}", MessageSource.Server);
+                try
+                {
+                    while (!token.IsCancellationRequested)
+                    {
+                        TcpClient client = server.AcceptTcpClient();
+                        NetworkStream clientStream = client.GetStream();
+                        if (server.Pending())
+                        {
+                            using (StreamReader reader = new StreamReader(clientStream))
+                            {
+                                string message = reader.ReadToEnd();
+                                await WriteToSummary(message, MessageSource.Client);
+                            }
+                        }
+
+                        if (pendingMessageToSend)
+                        {
+                            using (StreamWriter writer = new StreamWriter(clientStream))
+                            {
+                                writer.Write(pendingMessage ?? "");
+                                writer.Flush();
+                            }
+                        }
+
+                        await WriteToSummary(pendingMessage, MessageSource.Server);
+
+                        client.Close();
+
+                        Thread.Sleep(10);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    await WriteToSummary($"Error! Connections are no longer being accepted. Details: {ex.Message}.", MessageSource.Server);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "TCP server error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                server?.Stop();
+                server = null;
+            }
+        }
+
+        private async Task WriteToSummary(string message, MessageSource source)
+        {
+            await Dispatcher.BeginInvoke((Action)(() => Txt_Summary.AppendText($"[{source} ({DateTime.Now.ToLongTimeString()})]: {message}{Environment.NewLine}")));
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            server?.Stop();
+        }
+
+        private void Btn_Send_Click(object sender, RoutedEventArgs e)
+        {
+            pendingMessageToSend = true;
+            pendingMessage = Txt_Message.Text;
         }
     }
 
@@ -59,5 +161,11 @@ namespace TcpServerApplication
     {
         public ServerStatusEventsArgs(bool enabled) => CurrentStatus = enabled;
         public bool CurrentStatus { get; set; }
+    }
+
+    enum MessageSource
+    {
+        Server,
+        Client
     }
 }
