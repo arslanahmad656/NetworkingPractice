d4ab1764ef62fbf2e5be6e647323c8a3b5c446ec
Arslan Ahmad <ars.ahm6@gmail.com>
2019-04-26 20:18:58 +0500

    Basic layout ready for TCP server

---
+++ Networking/TcpServerApplication/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace TcpServerApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private event EventHandler<ServerStatusEventsArgs> OnServerStatusChanged;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            OnServerStatusChanged += (senderObj, args) => SetControlsStatus(args.CurrentStatus);
+        }
+
+        private void SetControlsStatus(bool serverStarted)
+        {
+            Txt_Port.IsEnabled = !serverStarted;
+            Btn_Start.IsEnabled = !serverStarted;
+            Btn_Stop.IsEnabled = serverStarted;
+            Txt_Message.IsEnabled = serverStarted;
+            Btn_Send.IsEnabled = serverStarted;
+        }
+
+        private void Btn_Start_Click(object sender, RoutedEventArgs e)
+        {
+            OnServerStatusChanged?.Invoke(new object(), new ServerStatusEventsArgs(true));
+        }
+
+        private void Btn_Stop_Click(object sender, RoutedEventArgs e)
+        {
+            OnServerStatusChanged?.Invoke(new object(), new ServerStatusEventsArgs(false));
+        }
+    }
+
+    class ServerStatusEventsArgs : EventArgs
+    {
+        public ServerStatusEventsArgs(bool enabled) => CurrentStatus = enabled;
+        public bool CurrentStatus { get; set; }
+    }
+}
