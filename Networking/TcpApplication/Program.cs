128cb5c52018b2f354197af07c5a5890cd93c79c
Arslan Ahmad 656 <arslan_mgl6445@live.com>
2019-04-28 10:44:09 +0500

    Running TCP application project added.

---
+++ Networking/TcpApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TcpApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t1 = Task.Run(() => ServerRunner.Run());
+            Thread.Sleep(2000);
+            var t2 = Task.Run(() => ClientRunner.Run("127.0.0.1", "Hello from server"));
+
+            Task.WaitAll(new[] { t1, t2 });
+            Console.ReadKey();
+        }
+    }
+}
